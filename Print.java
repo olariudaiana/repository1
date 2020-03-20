69c5e84de215cb5a031916fa1881e88f6553e64a
Daiana Olariu <daiana.olariu519@yahoo.com>
2020-03-20 04:18:35 +0200

    Initial commit

---
+++ Print.java
@@ -0,0 +1,16 @@
+package loose.oose.fis;
+
+import loose.oose.fis.documents.Document;
+import loose.oose.fis.documents.JSON;
+import loose.oose.fis.documents.XML;
+import loose.oose.fis.processors.Procesor;
+import loose.oose.fis.processors.ProcesorCautare;
+import loose.oose.fis.processors.ProcesorCompus;
+
+
+public class Print{
+
+System.out.println("Class Print!");
+
+
+}
